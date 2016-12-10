66b5b4a5181c21dacc302a066ae271cbc7390a77
Julia Tacla <juju.tacla@gmail.com>
2016-12-10 15:15:30 -0200

    criamos a rota e botao para o formulario

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-	resources :animals, only: [:index, :show]
+	resources :animals, only: [:index, :show] do
+		resources :fosters, only: [:new]
+	end
 end
